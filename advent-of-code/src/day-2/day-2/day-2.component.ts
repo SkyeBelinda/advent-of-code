a766c57ccdb5dc0fa7becc1d74f11bc32acbbc9e
Skye Simpson <ssimpson@TradeMe.local>
2018-12-04 11:12:29 +1300

    day 2 set up

---
+++ advent-of-code/src/day-2/day-2/day-2.component.ts
@@ -0,0 +1,7 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-day-2',
+    templateUrl: './day-2.component.html'
+})
+export class Day2Component { }
--- advent-of-code/src/day-2/day-2/day-2.module.ts
+++ advent-of-code/src/day-2/day-2/day-2.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Day2Component } from './day-2.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    Day2Component
+  ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [
+    Day2Component
+  ],
+  bootstrap: [Day2Component]
 })
 export class Day2Module { }
