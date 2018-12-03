eb1e2ea945c24b3373c49c2a2f67106038995d84
Skye Simpson <ssimpson@TradeMe.local>
2018-12-03 16:58:09 +1300

    result 2

--- advent-of-code/src/app/app.component.ts
+++ advent-of-code/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { input } from './input';
+import { of, from } from 'rxjs';
+import { map, reduce, mergeMap, tap, scan, first, pairwise, take, repeat, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,16 +9,49 @@ import { input } from './input';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'advent-of-code';
-  result: number;
+  public title = 'advent-of-code';
+  public result1 = 0;
+  public result2 = 0;
+  public formatToArrayPart1 = input.split('\n').map(Number);
 
   ngOnInit(): void {
     this.getTheStartingFrequency();
+    this.getTheFirstRepeatedNumber();
   }
 
   public getTheStartingFrequency() {
     const formatToArrayPart1 = input.split('\n').map(Number);
-     console.log(formatToArrayPart1);
-    this.result = formatToArrayPart1.reduce((prev, frequency) => prev + frequency, 0);
+    this.result1 = formatToArrayPart1.reduce((prev, frequency) => prev + frequency, 0);
+  }
+
+  public getTheFirstRepeatedNumber() {
+    const dataStream = of(input).pipe(
+      repeat(),
+      map(item => item.split('\n')),
+      mergeMap(arr => from(arr)),
+      // tslint:disable-next-line:radix
+      map(val => parseInt(val)),
+    );
+
+    const values = [0];
+
+    const part2 = dataStream.pipe(
+      scan((acc, val) => {
+        acc += val;
+        const duplicate = values.indexOf(acc) >= 0;
+        if (duplicate) {
+          throw new Error(`${acc}`);
+        }
+        return acc;
+      }, 0),
+      tap(value => values.push(value)),
+    );
+
+    part2.subscribe(
+      null,
+      e => {
+        return console.log('FOUND', e.message),
+        this.result2 = e.message;
+      });
   }
 }
