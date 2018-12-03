e63ed2e59962025c9e5e0192bdedd7046e82bfaf
Skye Simpson <ssimpson@TradeMe.local>
2018-12-03 16:00:07 +1300

    part one answer working

--- advent-of-code/src/app/app.component.ts
+++ advent-of-code/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { input } from './input';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'advent-of-code';
+  result: number;
+
+  ngOnInit(): void {
+    this.getTheStartingFrequency();
+  }
+
+  public getTheStartingFrequency() {
+    const formatToArrayPart1 = input.split('\n').map(Number);
+     console.log(formatToArrayPart1);
+    this.result = formatToArrayPart1.reduce((prev, frequency) => prev + frequency, 0);
+  }
 }
